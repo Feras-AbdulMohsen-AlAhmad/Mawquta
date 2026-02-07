4d2223b4c7ef00cdbe03d7bbdc65cf192f8b0dd5
Firas AL-Ahmad <w.firas.f@gmail.com>
2026-02-08 01:08:30 +0300

    feat: add BIG_DATA_CLOUD_API endpoint to configuration

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,6 @@
 export const CONFIG = {
   BASE_URL: "https://api.aladhan.com/v1",
+  BIG_DATA_CLOUD_API: "https://api.bigdatacloud.net/data",
   METHOD: 2,
   DEFAULT_CITY: "Damascus",
   DEFAULT_COUNTRY: "Syria",
